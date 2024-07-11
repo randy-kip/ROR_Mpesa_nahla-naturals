5d6a440b708a2df6fad54e5dbc622e5e1c86e10e
Randy Kipkurui <randy.kipkurui4@gmail.com>
2024-07-11 09:58:17 +0300

    🙈ignore: ignore the local env file.
    🐛fix: update the MPesa Controller to allow env access from Render deployment.

--- app/controllers/mpesas_controller.rb
+++ app/controllers/mpesas_controller.rb
@@ -2,6 +2,16 @@
 class MpesasController < ApplicationController
     require 'rest-client' # Include the rest-client gem for making HTTP requests
     rescue_from SocketError, with: :OfflineMode # Rescue from SocketError and handle it with the OfflineMode method
+
+    mpesa_consumer_key: 'RfqDHYVDbtAyM31p0o1XJoMdtGrQxRpQzxoAGAJ07tublVBk'
+    mpesa_consumer_secret: 'axcsqZ3G0up1bYArba2vmlO7ss4N0tkCsn7KRFHj53GjtZWnEseDIGR8GzcvYh25'
+    mpesa_passkey= 'bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919'
+    mpesa_initiator_username: 'nahla-api'
+    mpesa_initiator_security_credential: 'Safaricom999!*!'
+    callback_url: 'https://3199-41-139-192-167.ngrok-free.app'
+    query_url: 'https://sandbox.safaricom.co.ke/mpesa/stkpushquery/v1/query'
+    register_url: 'https://sandbox.safaricom.co.ke/mpesa/c2b/v1/registerurl'
+    mpesa_shortcode: '174379'
   
     def stkpush
       phoneNumber = params[:phoneNumber] # Get the phone number from the request parameters
@@ -9,8 +19,8 @@ class MpesasController < ApplicationController
   
       url = "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest" # URL for initiating an STK push
       timestamp = "#{Time.now.strftime "%Y%m%d%H%M%S"}" # Generate a timestamp
-      business_short_code = ENV["MPESA_SHORTCODE"] # Get the Mpesa business short code from environment variables
-      password = Base64.strict_encode64("#{business_short_code}#{ENV["MPESA_PASSKEY"]}#{timestamp}") # Generate the password for the request
+      business_short_code = mpesa_shortcode # Get the Mpesa business short code from environment variables
+      password = Base64.strict_encode64("#{business_short_code}#{mpesa_passkey}#{timestamp}") # Generate the password for the request
   
       # Payload for the STK push request
       payload = {
@@ -22,7 +32,7 @@ class MpesasController < ApplicationController
         'PartyA': phoneNumber,
         'PartyB': business_short_code,
         'PhoneNumber': phoneNumber,
-        'CallBackURL': "#{ENV["CALLBACK_URL"]}/callback_url", # URL for receiving callback from Mpesa
+        'CallBackURL': "#{callback_url}/callback_url", # URL for receiving callback from Mpesa
         'AccountReference': 'Nahla Naturals',
         'TransactionDesc': "Nahla Naturals Essentials"
       }.to_json
@@ -60,10 +70,10 @@ class MpesasController < ApplicationController
       checkoutId = params[:checkoutRequestID] # Get the checkout request ID from the request parameters
   
       timestamp = "#{Time.now.strftime "%Y%m%d%H%M%S"}" # Generate a timestamp
-      business_short_code = ENV["MPESA_SHORTCODE"] # Get the Mpesa business short code from environment variables
-      password = Base64.strict_encode64("#{business_short_code}#{ENV["MPESA_PASSKEY"]}#{timestamp}") # Generate the password for the request
+      business_short_code = mpesa_shortcode # Get the Mpesa business short code from environment variables
+      password = Base64.strict_encode64("#{business_short_code}#{mpesa_passkey}#{timestamp}") # Generate the password for the request
   
-      url = ENV['QUERY_URL'] # URL for querying payment status
+      url = query_url # URL for querying payment status
   
       # Headers for the query request
       headers = {
@@ -107,8 +117,8 @@ class MpesasController < ApplicationController
     def generate_access_token_request
       @url = "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials" # URL for generating access token
   
-      @consumer_key = ENV['MPESA_CONSUMER_KEY'] # Get the consumer key from environment variables
-      @consumer_secret = ENV['MPESA_CONSUMER_SECRET'] # Get the consumer secret from environment variables
+      @consumer_key = mpesa_consumer_key # Get the consumer key from environment variables
+      @consumer_secret = mpesa_consumer_secret # Get the consumer secret from environment variables
       @userpass = Base64::strict_encode64("#{@consumer_key}:#{@consumer_secret}") # Generate the base64 encoded user:pass
       @headers = {
         Authorization: "Bearer #{@userpass}" # Set the Authorization header
